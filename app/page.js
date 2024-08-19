5bc196bed77dad93304ecef7b86438bec26ebf64
mohhaych <mohamed54641@gmail.com>
2024-08-19 13:53:59 +0100

    Update navigation styles and links in Nav component

--- app/page.js
+++ app/page.js
@@ -1,23 +1,21 @@
 "use client";
-import Nav from "@/components/nav";
-import { signIn, signOut, useSession } from "next-auth/react";
+
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn ('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
-        </div>
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hello, <b>{session?.user?.name}</b>
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt={''} className="w-6 h-6" />
+        <span className="px-2">
+          {session?.user?.name}
+        </span>
       </div>
-    );
-  }
-
-  return (
-    <div className="bg-blue-900 min-h-screen">
-      <Nav />
-      <div>logged in {session.user.email}</div>
     </div>
-  );
+  </Layout>
 }
