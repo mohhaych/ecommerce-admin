1adc364c991914abd00015716711bb0384fd1df7
mohhaych <mohamed54641@gmail.com>
2024-08-30 09:36:32 +0100

    Update dependencies and add image upload functionality to ProductForm.js

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -8,7 +8,8 @@ export default function ProductForm({
     title: existingTitle = '', 
     description: existingDescription = '', 
     price: existingPrice = '', 
-    _id: productId = ''  // Add productId as a prop
+    _id: productId = '',  // Add productId as a prop
+    images: existingImages,
 }) {
     const [title, setTitle] = useState(existingTitle); // Initialize state with props
     const [description, setDescription] = useState(existingDescription);
@@ -16,6 +17,7 @@ export default function ProductForm({
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
     const [goToProducts, setGoToProducts] = useState(false);
+    const [images, setImages] = useState([existingImages || []]);
 
     const router = useRouter();
 
@@ -52,6 +54,20 @@ export default function ProductForm({
         }
     }, [goToProducts, router]);
 
+    async function uploadImages(ev) {
+        const files = ev.target?.files;
+        if (files?.length > 0) {
+            const data = new FormData();
+            for (const file of files) {
+                data.append('file', file);
+            }
+            const res = await axios.post('/api/upload', data);
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links];
+            });
+        }
+    }
+
     return (
         <form onSubmit={handleSubmit}>
             <label>Product name</label>
@@ -62,6 +78,25 @@ export default function ProductForm({
                 onChange={ev => setTitle(ev.target.value)}
                 required
             />
+            <label>
+                Photos
+            </label>
+            <div className='mb-2'>
+                <label className='w-24 h-24 cursor-pointer text-center 
+                flex items-center justify-center text-sm gap-1
+                text-gray-500 rounded-lg bg-gray-200'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                    </svg>
+                    <div>
+                        Upload
+                    </div>
+                    <input type='file' onChange={uploadImages} className='hidden'/>
+                </label>
+                {!images?.length && (
+                    <div>No photos in this product</div>
+                )}
+            </div>
             <label>Description</label>
             <textarea 
                 placeholder="Description" 
